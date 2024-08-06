06306c721c847e718aee4a5ec830fb247d8a7634
Savar_ C0002_Vas <Savar.C0002@gmail.com>
2024-08-06 15:47:45 +0530

    Revert "Keeping One Message"
    This reverts commit 1de1df4977defb7069b28098720ab158bed3c7ac.

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy! Welcome To Batch 7!");
+Console.WriteLine("Hello, C# Academy! Welcome To Batch 7!");
+Console.WriteLine("Hello, C# Academy! Welcome To Batch 7!");
+Console.WriteLine("Hello, C# Academy! Welcome To Batch 7!");
+Console.WriteLine("Hello, C# Academy! Welcome To Batch 7!");
