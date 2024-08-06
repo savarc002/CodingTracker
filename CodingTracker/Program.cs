1819db78c09d864d3589d7e2adac4c5b0128f1d1
Savar_ C0002_Vas <Savar.C0002@gmail.com>
2024-08-06 16:03:15 +0530

    Initial Commit.

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,3 +1,9 @@
 ﻿MyMethod();
+MyMethod1();
+MyMethod2();
+MyMethod3();
 
 void MyMethod() { Console.WriteLine("I created a New Method"); }
+void MyMethod1() { Console.WriteLine("I created a New Method1"); }
+void MyMethod2() { Console.WriteLine("I created a New Method2"); }
+void MyMethod3() { Console.WriteLine("I created a New Method3"); }
