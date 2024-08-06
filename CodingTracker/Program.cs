2a3a78e9726137787c2d99993397d46f4eb23126
Savar_ C0002_Vas <Savar.C0002@gmail.com>
2024-08-06 15:50:46 +0530

    I Wrote a New Method.

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,6 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy! Welcome To Batch 7!");
-Console.WriteLine("Hello, C# Academy! Welcome To Batch 7!");
-Console.WriteLine("Hello, C# Academy! Welcome To Batch 7!");
-Console.WriteLine("Hello, C# Academy! Welcome To Batch 7!");
-Console.WriteLine("Hello, C# Academy! Welcome To Batch 7!");
+﻿MyMethod();
+void MyMethod()
+{
+    Console.WriteLine("Now I have a New Method");
+}
